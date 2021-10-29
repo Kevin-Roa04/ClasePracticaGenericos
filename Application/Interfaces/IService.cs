2e2a2e34565dce36e18a1ed97c9f885a3efbfcba
Kevin-Roa04 <kevin.roa49u@std.uni.edu.ni>
2021-10-29 16:20:28 -0600

    Subiendo la clase practica

---
+++ Application/Interfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Interfaces
+{
+    public interface IService<T>
+    {
+        void Create(T t);
+        int Update(T t);
+        bool Delete(T t);
+        T[] FindAll();
+        int GetLastId();
+        void Add(T t, ref T[] data);
+        void Ordenar(T[] t, IComparer<T> comparador);
+    }
+}
---
+++ Domain/Interfaces/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IModel<T>
+    {
+        void Create(T t);
+        int Update(T t);
+        bool Delete(T t);
+        T[] FindAll();
+        int GetLastId();
+        void Add(T t, ref T[] data);
+        void Ordenar(T[] t, IComparer<T> comparador);
+    }
+}
